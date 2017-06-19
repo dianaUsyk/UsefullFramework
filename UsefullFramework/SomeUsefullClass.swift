519c679a75dd412121ea56a67a396f1d44c931e5
diana_usyk <Diana.Usik@innovecs.com>
2017-06-19 18:03:52 +0300

    Init commit

---
+++ UsefullFramework/SomeUsefullClass.swift
@@ -0,0 +1,19 @@
+//
+//  SomeUsefullClass.swift
+//  UsefullFramework
+//
+//  Created by diana.usik on 6/19/17.
+//  Copyright © 2017 diana.usik. All rights reserved.
+//
+
+import Foundation
+
+public class SomeUsefullClass {
+    
+    var name: String = "My God"
+    
+    public func greatMe() {
+        
+        print("Hello, \(name)")
+    }
+}
---
+++ UsefullFramework/UsefullFramework.h
@@ -0,0 +1,19 @@
+//
+//  UsefullFramework.h
+//  UsefullFramework
+//
+//  Created by diana.usik on 6/19/17.
+//  Copyright © 2017 diana.usik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UsefullFramework.
+FOUNDATION_EXPORT double UsefullFrameworkVersionNumber;
+
+//! Project version string for UsefullFramework.
+FOUNDATION_EXPORT const unsigned char UsefullFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UsefullFramework/PublicHeader.h>
+
+
